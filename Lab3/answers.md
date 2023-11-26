00f076eb20ed9943b8faa4bf14212aaee8364f62
justineclaire <langridge.justine@gmail.com>
2023-11-26 01:08:23 +0000

    answer1

---
+++ Lab3/answers.md
@@ -0,0 +1,11 @@
+# Question 1
+**Explain what is meant by the stream abstraction. What is the relationship between streams and the observer pattern? What are streams useful for modeling and when might you use them in Rich Web development?**
+A stream is data that is created, processed, and consumed in an incremental fashion. It is an abstraction for some data which may or may not be present now or may or may not arrive in the future. The data reaches the client piece by piece e.g. streaming videos on Youtube. 
+Streams (also called observables) have relationship with 'observers'. These observers 'subscribe' to the stream and can be used to do something with the data passing through the stream. They take and nanipulate the data.
+Streams are useful for modeling because they help you model asynchronous operations in an easier to manage way and you may use them when dealing with asynchronous events and we want to increase performance as the client wont need to wait for a whole file to download in order to have access to it. 
+# Question 2
+**Assume that you are building an interface to an API in your Rich Web App. Describe in detail how you could use the RxJS library to handle asynchronous network responses to API requests. In your opinion, what are the benefits to using a streams library for networking over, say, promises? And what do you think are the downsides?**
+
+# Question 3
+**Consider three asynchronous tasks, A,B & C. What are the consequences of these functions sharing global state? What is a good practice to alleviate any problems associated with this?**
+
--- Lab3/problem-1/scripts.js
+++ Lab3/problem-1/scripts.js
@@ -1,20 +1,15 @@
-import { map } from '../node_modules/rxjs';
+document.addEventListener('DOMContentLoaded', () => {
+const { fromEvent, map, find } = rxjs;  // import pipe functions here
+ 
 
 //query selectors
 const noteContainer = document.querySelector(".note-container");
 const form = document.querySelector('form');
+const button = document.getElementById('ibutton');
 const titleinput = document.querySelector('#title');
-const notebutton = document.getElementById('button');
+const noteinput = document.querySelector('#note');
+const colourinput = document.querySelector('#colour').value;
 
-//class for new note
-class Note {
-    constructor(title, body, colour) {
-        this.title = title;
-        this.body = body;
-        this.colour = colour;
-        this.id = Math.random();
-    }
-}
 
 //Show alert message
 function showAlertMessage(message, alertClass){
@@ -27,66 +22,7 @@ function showAlertMessage(message, alertClass){
 }
 
 
-//remove a note from storage
-function removeNote(id) {
-    const notes = getNotes();
-    notes.forEach((note, index) => {
-        if(note.id === id){
-            notes.splice(index, 1);
-        }
-        localStorage.setItem('noteApp.notes', JSON.stringify(notes));
-    })
-}
-// create note for user
-function addNotetoList(note){
-    const newUINote = document.createElement('div');
-    newUINote.classList.add('note');
-    newUINote.classList.add(note.colour);
-    newUINote.innerHTML = `
-    <span hidden>${note.id}</span>
-    <h2 class='note-title'>${note.title}</h2>
-    <p class='note-title' sty;e>${note.body}</p>
-    <div class="note-btns">
-        <button class="delete">delete</button>
-    </div>
-    `;
-    noteContainer.appendChild(newUINote);
-}
 
-
-
-//event note button
-noteContainer.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('delete')) {
-        const currentNote = e.target.closest('.note');
-        currentNote.remove();
-        showAlertMessage('Your note was permanently deleted', 'remove-message');
-
-        const id = currentNote.querySelector('span').textContent;
-        removeNote(Number(id));
-    }
-})
-
-const notesObservable = getNotes();
-displaynotesObservable = notesObservable.pipe( 
-    notesObservable.forEach(note => {addNotetoList(note)})
-).subscribe(
-    result => console.log(result),
-    error => console.error(error),
-    () => console.log('Complete')
-);
-
-//event display notes
-//document.addEventListener('DOMContentLoaded', displayNotes);
-
-//show notes to user
-/*function displayNotes() {
-    const notes = getNotes();
-    notes.forEach(note => {
-        addNotetoList(note);
-    })
-}
-*/
 //get notes from storage
 function getNotes() {
     let notes;
@@ -105,25 +41,107 @@ function addNotetoStorage(note) {
     localStorage.setItem('noteApp.notes', JSON.stringify(notes));
 }
 
+function removeAllNotes() {
+    localStorage.removeItem('noteApp.notes');
+}
 
-//event submit form
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+//remove a note from storage
+function removeNote() {
+    const notes = getNotes();
+    notes.forEach((note, index) => {
+        /*if(note.id === id){
+            notes.splice(index, 1);
+        }*/
+        notes.splice(index, 1);
+        localStorage.setItem('noteApp.notes', JSON.stringify(notes));
+    })
+}
 
-    const titleinput = document.querySelector('#title');
-    const noteinput = document.querySelector('#note');
-    const colourinput = document.querySelector('#colour').value;
+// create note for user
+function addNotetoList(note){
+    const newUINote = document.createElement('div');
+    newUINote.classList.add('note');
+    newUINote.classList.add(note.colour);
+    newUINote.innerHTML = `
+    <span hidden>${note.id}</span>
+    <h2 class='note-title'>${note.title}</h2>
+    <p class='note-body'>${note.note}</p>
+    <div class="note-btns">
+        <button class="delete">delete</button>
+    </div>
+    `;
+    noteContainer.appendChild(newUINote);
+}
 
-    //validate inputs
+//show notes to user
+function displayNotes() {
+    const notes = getNotes();
+    notes.forEach(note => {
+        addNotetoList(note);
+    })
+}
+
+//event delete note
+const deletePressed = fromEvent(noteContainer, 'click');
+
+const delNote = deletePressed.pipe(
+    map(event => {
+        // Assuming each note has an 'id' attribute
+        if(event.target.classList.contains('delete')){
+        const closestNote = event.target.closest('.note');
+        return closestNote;
+    }
+}));
+
+const deletePressed$ = delNote.subscribe(note => { 
+    if (note) {
+        showAlertMessage('Your note was permanently deleted', 'remove-message');
+        note.remove();
+        const id = currentNote.querySelector('span').textContent;
+        removeNote(id);
+        
+    } else {
+        console.log('No matching note found');
+    }
+});
+
+//event loaded
+const loaded = fromEvent(document, 'DOMContentLoaded');
+const loaded$ = loaded.subscribe(displayNotes());
+
+
+//event create note
+const submitPressed = fromEvent(button, 'click');
+const noteobj = submitPressed.pipe(
+    map(event => {
+        // Prevent the default form submission behavior
+        event.preventDefault();
+    
+        return {
+            title: titleinput.value,
+            note: noteinput.value,
+            colour: colourinput,
+            id: Math.random()
+        }
+    })
+)
+
+noteobj.subscribe(note => {
+    createAndHandleNote(note);
+});
+  
+  function createAndHandleNote(note) {
     if(titleinput.value.length > 0 && noteinput.value.length > 0){
-        const newNote = new Note(titleinput.value, noteinput.value, colourinput);
-        addNotetoList(newNote);
-        addNotetoStorage(newNote);
+        addNotetoList(note);
+        addNotetoStorage(note);
         titleinput.value = '';
         noteinput.value = '';
         showAlertMessage('Note successfully added', 'success-message');
         titleinput.focus();
+
   } else {
     showAlertMessage('Please add both a title and a note', 'alert-message');
-  }
+  }}
+
+
 });
